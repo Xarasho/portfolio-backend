0933a52133c4d20a060f0311b8259bf08159a767
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-01 17:54:56 -0600

    Finished working on the Activity module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileModule } from './profile/profile.module';
 import { ResumeModule } from './resume/resume.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [ProfileModule, ResumeModule],
+  imports: [ProfileModule, ResumeModule, ActivityModule],
   controllers: [],
   providers: [],
 })
