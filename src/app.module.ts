7c05ee63e1ec88e0576e967ec9d1b041f998ae52
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-01 18:37:24 -0600

    Finished working on the SoftSkill module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfileModule } from './profile/profile.module';
 import { ResumeModule } from './resume/resume.module';
 import { ActivityModule } from './activity/activity.module';
+import { SoftSkillModule } from './soft-skill/soft-skill.module';
 
 @Module({
-  imports: [ProfileModule, ResumeModule, ActivityModule],
+  imports: [ProfileModule, ResumeModule, ActivityModule, SoftSkillModule],
   controllers: [],
   providers: [],
 })
