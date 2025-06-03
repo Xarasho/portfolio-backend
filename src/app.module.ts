a8191fd28a1d5d46af152d67970613689b4c561e
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-02 21:10:24 -0600

    Finished working on the Message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { LanguageModule } from './language/language.module';
 import { SkillModule } from './skill/skill.module';
 import { ProjectModule } from './project/project.module';
 import { BlogModule } from './blog/blog.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [ProfileModule, ResumeModule, ActivityModule, SoftSkillModule, EducationModule, ExperienceModule, LanguageModule, SkillModule, ProjectModule, BlogModule],
+  imports: [ProfileModule, ResumeModule, ActivityModule, SoftSkillModule, EducationModule, ExperienceModule, LanguageModule, SkillModule, ProjectModule, BlogModule, MessageModule],
   controllers: [],
   providers: [],
 })
---
+++ src/message/message.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class MessageService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  create(createMessageDto: CreateMessageDto) {
+    return this.message.create({
+      data: createMessageDto,
+    });
+  }
+
+  findAll() {
+    return this.message.findMany({ orderBy: { createdAt: 'desc' } });
+  }
+
+  findOne(id: string) {
+    return this.message.findFirst({ where: { id } });
+  }
+}
